cdff829e39dffeb3556050e351b23341eb330e41
hanskkangg <hans.kkang@gmail.com>
2024-02-07 17:46:47 -0500

    Commit 11  - flag turning

--- app/src/main/java/algonquin/cst2335/kang0057/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/kang0057/MainActivity.java
@@ -18,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+sw = findViewById(R.id.languageSwitch);
+imgView=findViewById(R.id.imageView2);
         sw.setOnCheckedChangeListener((btn, isChecked) -> {
 
             if (isChecked) {
