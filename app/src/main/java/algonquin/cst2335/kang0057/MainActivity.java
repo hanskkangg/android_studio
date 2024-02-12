13de73a3cd9e1b9b3003833d2185159a8ca89fa4
hanskkangg <hans.kkang@gmail.com>
2024-02-12 17:29:04 -0500

    Commit 12  - Added Log.w and Log.d commments (Lab4)

--- app/src/main/java/algonquin/cst2335/kang0057/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/kang0057/MainActivity.java
@@ -3,6 +3,7 @@ package algonquin.cst2335.kang0057;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
@@ -11,15 +12,21 @@ import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Declaring the TAG variable for logging
+    private static final String TAG = "MainActivity";
+
     ImageView imgView;
     Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-sw = findViewById(R.id.languageSwitch);
-imgView=findViewById(R.id.imageView2);
+        // Using the TAG variable for logging
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        sw = findViewById(R.id.languageSwitch);
+        imgView=findViewById(R.id.imageView2);
         sw.setOnCheckedChangeListener((btn, isChecked) -> {
 
             if (isChecked) {
@@ -33,6 +40,48 @@ imgView=findViewById(R.id.imageView2);
             } else {
                 imgView.clearAnimation();
             }
+
         });
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Example of using TAG in another lifecycle method
+        Log.d(TAG, "onStart called");
+        Log.w(TAG, "onStart(): The activity is about to become visible.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+        // Log message for onResume
+        Log.w(TAG, "onResume(): The activity has become visible (it is now resumed).");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called"); // Log message for onPause
+        Log.w(TAG, "onPause(): Another activity is taking focus (this activity is about to be paused).");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");   // Log message for onStop
+        Log.w(TAG, "onStop(): The activity is no longer visible (it is now stopped).");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
+        Log.w(TAG, "onDestroy(): The activity is about to be destroyed.");
     }
 }
